0ecacf17d110774e0dd7c32df315f589e1b00d8f
Philipp Scheit <p.scheit@ps-webforge.com>
2014-05-26 08:58:00 +0200

    use native cucumber (not infected) to test cuked zombie

--- tests/js/cucumber/stepDefinitions.js
+++ tests/js/cucumber/stepDefinitions.js
@@ -6,38 +6,83 @@ module.exports = function() {
   var os = require('os');
   var _ = require('lodash');
   var glob = require('glob');
+
   var chai = require('chai');
   chai.config.includeStack = true;
-
   chai.use(require('./chai-file-and-directory'));
+  var expect = chai.expect;
 
-  var cukedZombie = require('../../../');
-
-  var World = cukedZombie.infectWorld(cucumberStep, {
-    cli: [__dirname, '..', '..', '..', 'bin', 'cli.' +(os.platform() === 'win32' ? 'bat' : 'sh')].join(path.sep),
-    domains: {
-      'pegasus.ps-webforge.net': 'ssc-testing.ps-webforge.com',
-      'psc-laptop': 'ssc.laptop.ps-webforge.net',
-      'psc-desktop': 'ssc.desktop.ps-webforge.net',
-      'addorange-macbook': 'ssc.dev.192.168.2.222.xip.io'    
-    },
-    cookies: [{
-      name: 'staging_access',
-      value: 'tokenU1V2pUK'
-    }]
-  });
-
-  World.prototype.init = function() {
+  this.World = function(callback) {
     this.env = {};
+
+    callback();
   };
+  
 
-  var commons = {};
+  this.Given(/^I am using the project "([^"]*)"$/, function(name, callback) {
+    var path = require('path');
 
-  _.each(glob.sync('./stepDefinitions/*step-definitions.js', { cwd: __dirname }), function(file) {
-    cukedZombie.infectStep(cucumberStep, require(file), { arguments: [chai.expect, commons, chai] });
+    var project = {
+      name: name,
+      dir: path.resolve('./tests/files/'+name),
+      getFile: function(relativePath) {
+        return path.resolve(project.dir, relativePath);
+      },
+      getDirectory: function(relativePath) {
+        return path.resolve(project.dir, relativePath);
+      }
+    };
+
+    expect(project.dir).to.be.an.existingDirectory;
+
+    this.env.project = project;
+    callback();
   });
 
-  this.Before("@mailing", function(callback) {
-    this.clearMailSpool(callback);
+  this.Given(/^I have created my step\-definition bootstrap by hand$/, function(callback) {
+    expect(this.env.project.getFile('tests/js/cucumber/bootstrap.js')).to.be.an.existingFile;
+
+    callback();
+  });
+
+  this.Given(/^I have installed cuked\-zombie with npm$/, function(callback) {
+    expect(this.env.project.getDirectory('node_modules/cuked-zombie')).to.be.an.existingDirectory;
+    expect(this.env.project.getDirectory('node_modules/grunt-cucumber')).to.be.an.existingDirectory;
+
+    callback();
+  });  
+
+  this.When(/^I execute the grunt cucumber task with filter "([^"]*)" in the root of the project$/, function(filter, callback) {
+    var env = this.env;
+    var exec = require('child_process').exec;
+    var grunt = require('grunt');
+    var path = require('path');
+    var os = require('os');
+
+    var task = exec('grunt cucumber --stack --no-color --filter '+filter, { cwd: this.env.project.dir }, function(error, stdout, stderr) {
+      expect(error, "grunt cucumber produces an error:\n"+stdout+stderr).to.be.null;
+
+      env.cucumberRun = {
+        stderr: stderr,
+        stdout: stdout.replace(/\033\[[0-9;]*m/g, '') // strip ansi until cucumber has a --no-color switch
+      };
+    });
+
+    task.on('close', function (code) {
+      env.cucumberRun.exitCode = code;
+      callback();
+    });
+  });
+
+  this.Then(/^cucumber has run the feature "([^"]*)" successfully$/, function(arg1, callback) {
+    expect(this, 'cucumberRun not exited correctly').to.have.deep.property('env.cucumberRun.exitCode', 0);
+    
+
+    callback();
+  });
+
+  this.Then(/^all cucumber tests are passed$/, function(callback) {
+    expect(this).to.have.deep.property('env.cucumberRun.stdout').to.contain('6 steps (6 passed)').and.to.contain('1 scenario (1 passed)');
+    callback();
   });
 };
